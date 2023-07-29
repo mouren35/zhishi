65b8e6707db0cc6d53a485ba3811aa48fca9a437
mouren35 <85747566+mouren35@users.noreply.github.com>
2023-07-29 19:49:15 +0800

    2023/7/29

--- imports/ui/page/Home/index.jsx
+++ imports/ui/page/Home/index.jsx
@@ -1,48 +1,163 @@
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import { makeStyles } from '@material-ui/core/styles';
-import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import * as React from 'react';
+import {
+  AppBar,
+  Box,
+  Drawer,
+  Fab,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography
+} from '@mui/material';
+import React, { useEffect, useState } from 'react';
 
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-  },
-});
+import AddIcon from '@mui/icons-material/Add';
+import MenuIcon from '@mui/icons-material/Menu';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import SearchIcon from '@mui/icons-material/Search';
+import KnowlengePoint from '../KnowledgePoint';
 
-export default function Home() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+const options = [
+  'None',
+  'Atria',
+  'Callisto',
+  'Dione',
+  'Ganymede',
+  'Hangouts Call',
+  'Luna',
+  'Oberon',
+  'Phobos',
+  'Pyxis',
+  'Sedna',
+  'Titania',
+  'Triton',
+  'Umbriel',
+];
+
+const ITEM_HEIGHT = 48;
+
+
+function Home() {
+  const [isKeyboardOpen, setIsKeyboardOpen] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isKeyboardOpen = window.innerHeight < window.outerHeight;
+      setIsKeyboardOpen(isKeyboardOpen);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
 
   return (
-    <div style={{ height: '100vh' }}>
+    <div>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            知识
-          </Typography>
+          <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer}>
+            <MenuIcon />
+          </IconButton>
+          <Box>
+            <Typography variant="h6">
+              AnkiDroid
+            </Typography>
+            <Typography variant="subtitle2">
+              xxx张卡片待复习（）
+            </Typography>
+          </Box>
+          <div style={{ marginLeft: 'auto' }}>
+            <IconButton color="inherit" aria-label="search">
+              <SearchIcon />
+            </IconButton>
+            <IconButton color="inherit" aria-label="refresh">
+              <RefreshIcon />
+            </IconButton>
+            <IconButton
+              color='inherit'
+              aria-label="more"
+              id="long-button"
+              aria-controls={open ? 'long-menu' : undefined}
+              aria-expanded={open ? 'true' : undefined}
+              aria-haspopup="true"
+              onClick={handleClick}
+            >
+              <MoreVertIcon />
+            </IconButton>
+            <Menu
+              id="long-menu"
+              MenuListProps={{
+                'aria-labelledby': 'long-button',
+              }}
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              PaperProps={{
+                style: {
+                  maxHeight: ITEM_HEIGHT * 4.5,
+                  width: '20ch',
+                },
+              }}
+            >
+              {options.map((option) => (
+                <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleClose}>
+                  {option}
+                </MenuItem>
+              ))}
+            </Menu>
+          </div>
         </Toolbar>
       </AppBar>
 
-      <div style={{ height: '100%' }}>
-        111
+      <Drawer anchor="left" open={isDrawerOpen} onClose={toggleDrawer}>
+        <List>
+          <ListItem button>
+            <ListItemText primary="菜单项1" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="菜单项2" />
+          </ListItem>
+          <ListItem>
+            <ListItemText primary="菜单项3" />
+          </ListItem>
+        </List>
+      </Drawer>
+
+      <KnowlengePoint/>
+      <KnowlengePoint/>
+      <KnowlengePoint/>
+
+      <div style={{ position: 'fixed', bottom: isKeyboardOpen ? '72px' : '36px', right: '36px' }}>
+        <Fab color="primary" aria-label="add">
+          <AddIcon />
+        </Fab>
       </div>
 
-      <BottomNavigation
-        value={value}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-        showLabels
-        className={classes.root}
-      >
-        <BottomNavigationAction label="Recents" icon={<MenuBookIcon />} />
-        <BottomNavigationAction label="Favorites" icon={<FormatListBulletedIcon />} />
-      </BottomNavigation>
+      <footer style={{ display: 'flex', justifyContent: 'center', position: 'fixed', bottom: '6px', left: '0', right: '0' }}>
+        <p>今天学习xxx张卡片，用了xx分钟</p>
+      </footer>
     </div>
   );
-}
+};
+
+export default Home;
--- imports/ui/page/KnowledgePoint/index.jsx
+++ imports/ui/page/KnowledgePoint/index.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { List, ListItem, ListItemText, Collapse } from '@mui/material';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
 
-function KnowlengePoint () {
+function KnowlengePoint() {
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
     return (
         <div>
-            知识点
+            <List>
+                <ListItem button onClick={handleClick}>
+                    <ListItemText primary="可折叠标题" />
+                    {open ? <ExpandLess /> : <ExpandMore />}
+                    123
+                </ListItem>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        <ListItem button>
+                            <ListItemText primary="子项1" />
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemText primary="子项2" />
+                        </ListItem>
+                    </List>
+                </Collapse>
+            </List>
         </div>
     )
 }
